348b5435c5069cc989af97fc6a3b7807d1dec7be
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-24 04:12:32 +0200

    Update - front-end - by add login form n with style to login page and styles

--- code-mesh-react/src/page/Login.jsx
+++ code-mesh-react/src/page/Login.jsx
@@ -7,9 +7,23 @@ const Login = () => {
         <>
             <div class="main-login">
                 <div class="sub-login">
-                    
+                <h1>CodeMesh</h1>
+                    <div class="login-form">
+                        <div>
+                            <h3>Email</h3>
+                            <input type="text" placeholder="Enter your email"/>
+                        </div>
+                        <div>
+                            <h3>Password</h3>
+                            <input type="password" placeholder="Enter your Password"/>
+                        </div>
+                        <div>
+                            <button>Sign in</button>
+                            <br/>
+                            <a href="#">Sign up</a>
+                        </div>
+                    </div>
                 </div>
-
             </div>
         </>
     );
