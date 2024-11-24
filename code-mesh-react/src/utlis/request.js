840eb029b7b200ea8f5458acbe7cb4e0566e54c5
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-24 05:43:12 +0200

    Add - front-end - add request function

---
+++ code-mesh-react/src/utlis/request.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://localhost/learning/back/";
+
+export const requestAPI =  async({route,method = 'GET' ,body,header='Content-Type":"application/json'}) =>{
+    //route like api file php
+    //method post/get
+    //body is data
+    //ON PHP SET Authorization 
+    try{
+        const respons = await axios.request({
+            url:`${route}.php`,
+            method,
+            data:body,
+            headers:{
+                'Content-Type': header,
+                // "Content-Type":"application/json",  application/x-www-form-urlencoded
+                Authorization:localStorage.token,
+            }
+        });
+        return respons.data;
+
+    }catch (error){
+        return error;
+    }
+
+
+}
