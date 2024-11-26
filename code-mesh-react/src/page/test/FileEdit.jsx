b16dca2df5d6212a98e0b43ddcb2462d291ef7e7
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-26 03:52:40 +0200

    Update - front-end - update FileEdit.jsx by add handleContentChange function

--- code-mesh-react/src/page/test/FileEdit.jsx
+++ code-mesh-react/src/page/test/FileEdit.jsx
@@ -6,56 +6,79 @@ const FileEdit = () => {
     const fileId = '398756639';
     const filePath = 'yousif_mohammed_dahabra398756639/file.txt';
     const [content, setContent] = useState('');
+
     const getFileData = async () => {
         try {
-          const result = await requestAPI({
+        const result = await requestAPI({
             route: "get_file",
             method: "POST",
             body: { file_path: filePath },
-          });
+        });
     
-          console.log("Fetched file data:", result);
-          if (result.success) {
+        console.log("Fetched file data:", result);
+        if (result.success) {
             setContent(result.data.content);
-          } else {
+        } else {
             console.error("Error fetching file content:", result.message);
-          }
-        } catch (error) {
-          console.error("Error in getFileData:", error);
         }
-      };
-      useEffect(() => {
+        } catch (error) {
+        console.error("Error in getFileData:", error);
+        }
+    };
+
+    const handleContentChange = async (e) => {
+        const newContent = e.target.value; 
+        setContent(newContent);  
+    
+    
+        const result = await requestAPI({
+        route: "update_file",
+        method: "POST",
+        body: {
+            content: newContent,
+            file_path: filePath,
+        },
+        });
+    
+        console.log('File updated:', result);
+    };
+
+
+    useEffect(() => {
 
         getFileData();
         Pusher.logToConsole = true;//
         const pusher = new Pusher('221829f3a57f7bf42126', {
-          cluster: 'eu',
-          encrypted: true,
+        cluster: 'eu',
+        encrypted: true,
         });
-      
+    
         const channel = pusher.subscribe(`file.${fileId}`);
         channel.bind('EditFile', (data) => {
-          console.log('Received event data:', data);
-          setContent(data.content);
+        console.log('Received event data:', data);
+        setContent(data.content);
         });
-      
-        return () => {
-          channel.unbind_all();
-          channel.unsubscribe();
-        };
-      
     
-      }, [filePath]);
+        return () => {
+        channel.unbind_all();
+        channel.unsubscribe();
+        };
+    
+    
+    }, [filePath]);
+
+
+
     return (
         <div>
         <h2>Editing: {filePath}</h2>
         <textarea
-           value={content}
-
-          
-          style={{ width: '100%', height: '300px' }}
+        value={content}
+        onChange={handleContentChange}
+        
+        style={{ width: '100%', height: '300px' }}
         />
-      </div>
+    </div>
     );
 }
 export default FileEdit;
