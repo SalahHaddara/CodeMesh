b3efb5fba2c9c4618c370037f81c8e9389153fd3
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-26 03:02:47 +0200

    Update - back-end - api route file by add update_file and get_file

--- code-mesh-laravel/routes/api.php
+++ code-mesh-laravel/routes/api.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\AuthController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+
+//for test pusher
+Route::post('update_file', [FileController::class, 'update_content']);
+Route::post('get_file', [FileController::class, 'get_file_content']);
