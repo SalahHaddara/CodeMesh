db7f28ccf195edb7dd46f7cec3c633674164f19e
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-26 03:48:02 +0200

    Update - front-end - update FileEdit.jsx by add getFileData method

--- code-mesh-react/src/page/test/FileEdit.jsx
+++ code-mesh-react/src/page/test/FileEdit.jsx
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from 'react';
-const FileEdit = () => {
+import { requestAPI } from '../../utlis/request.js'
 
+const FileEdit = () => {
+    const [content, setContent] = useState('');
+    const getFileData = async () => {
+        try {
+          const result = await requestAPI({
+            route: "get_file",
+            method: "POST",
+            body: { file_path: filePath },
+          });
+    
+          console.log("Fetched file data:", result);
+          if (result.success) {
+            setContent(result.data.content);
+          } else {
+            console.error("Error fetching file content:", result.message);
+          }
+        } catch (error) {
+          console.error("Error in getFileData:", error);
+        }
+      };
+    
     return (
         <div>
         <h2>Editing: {filePath}</h2>
         <textarea
-           
+           value={content}
+
           
           style={{ width: '100%', height: '300px' }}
         />
