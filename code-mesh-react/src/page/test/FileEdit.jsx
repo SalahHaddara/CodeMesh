f427a9fedfbb9cafa98edc86189552e98f8e7ea2
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-26 03:44:20 +0200

    Update - front-end - update FileEdit.jsx by add textarea

--- code-mesh-react/src/page/test/FileEdit.jsx
+++ code-mesh-react/src/page/test/FileEdit.jsx
@@ -1,4 +1,15 @@
 import React, { useState, useEffect } from 'react';
 const FileEdit = () => {
+
+    return (
+        <div>
+        <h2>Editing: {filePath}</h2>
+        <textarea
+           
+          
+          style={{ width: '100%', height: '300px' }}
+        />
+      </div>
+    );
 }
 export default FileEdit;
