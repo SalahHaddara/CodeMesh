6e9c6c77134dcb6efb61519f8c0ea8dfbd1bacc8
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-26 08:35:56 +0200

    add store function to FileController

--- code-mesh-laravel/app/Http/Controllers/FileController.php
+++ code-mesh-laravel/app/Http/Controllers/FileController.php
@@ -16,6 +16,35 @@ class FileController extends Controller
         return response()->json(['files' => $files]);
     }
 
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'content' => 'required|string',
+            'language' => 'required|string',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+
+        $user = Auth::user();
+        $file_path = $user->base_folder . '/' . $request->name;
+
+        Storage::put($file_path, $request->content);
+
+        $file = File::create([
+            'user_id' => $user->id,
+            'name' => $request->name,
+            'file_path' => $file_path,
+            'language' => $request->language,
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'File created successfully',
+            'file' => $file
+        ], 201);
+    }
 }
 
