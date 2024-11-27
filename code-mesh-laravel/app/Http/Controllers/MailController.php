57d1d8589e743188d2789a7ae35006bb8b225261
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-27 22:38:39 +0200

    update - back-end - MailController

--- code-mesh-laravel/app/Http/Controllers/MailController.php
+++ code-mesh-laravel/app/Http/Controllers/MailController.php
@@ -10,14 +10,17 @@ class MailController extends Controller
 {
     public function sendInvitation()
     {
+        $request->validate([
+            'email' => 'required|email', 
+        ]);
         $details = [
             'title' => 'Welcome to Our Event!',
             'body' => 'We are thrilled to invite you to collaborate on our project. Please click on the following link.',
             'url' => 'http://127.0.0.1:8000/'
         ];
 
-        Mail::to('mohamaddmortada@gmail.com')->send(new InvitationMail($details));
+        Mail::to($request->input('email'))->send(new InvitationMail($details));
 
-        return response()->json(['message' => 'Invitation email sent!']);
+        return response()->json(['message' => 'Invitation email sent '. $request->input('email') . '!']);
     }
 }
