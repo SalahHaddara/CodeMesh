36031ac3bb73cc2fb9edd14a29d20fa78499e525
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-27 22:43:32 +0200

    update - front-end - MailInvitation.jsx fetch

--- code-mesh-react/src/page/MailInvitation.jsx
+++ code-mesh-react/src/page/MailInvitation.jsx
@@ -3,8 +3,23 @@ import axios from "axios";
 
 const Mailer = () => {
     const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
 
     const handleSendEmail = async () => {
+        if (!email) {
+            setMessage("Please enter a valid email address.");
+            return;
+        }
+
+        try {
+            const response = await axios.post("http://127.0.0.1:8000/api/invite", {
+                email: email,
+            });
+            setMessage(response.data.message);
+        } catch (error) {
+            console.error(error);
+            setMessage("Failed to send the invitation. Please try again.");
+        }
         
     };
 
