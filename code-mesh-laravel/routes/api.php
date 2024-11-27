83bcfbfa2a59b4967cf53f5dadb27aebe671034a
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-27 02:07:49 +0200

    add - back-end - Invitation-Route

--- code-mesh-laravel/routes/api.php
+++ code-mesh-laravel/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MailController;
 
+
+Route::post('/invite', [MailController::class, 'sendInvitation'])->withoutMiddleware(['auth:api']);
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
