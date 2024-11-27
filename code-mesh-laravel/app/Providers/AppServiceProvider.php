50f9fb80dbaf7b16c7af39dce0d5fc9a75a33a79
mohamaddmortada <mortada_mohamad@hotmail.com>
2024-11-27 02:52:50 +0200

    implemenet - back-end - Judge0Service

--- code-mesh-laravel/app/Providers/AppServiceProvider.php
+++ code-mesh-laravel/app/Providers/AppServiceProvider.php
@@ -6,17 +6,14 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
+  
+    public function register(): void {
+        $this->app->singleton(Judge0Service::class, function ($app) {
+            return new Judge0Service();
+        });
     }
 
-    /**
-     * Bootstrap any application services.
-     */
+    
     public function boot(): void
     {
         //
